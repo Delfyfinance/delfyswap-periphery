8ebefec277d54154a92774560772dedfb2b3b34e
unknown <botassammzy@gmail.com>
2021-02-20 23:58:45 -0500

    delfyswap-periphery

---
+++ contracts/examples/ExampleComputeLiquidityValue.sol
@@ -0,0 +1,90 @@
+pragma solidity =0.6.6;
+
+import '../libraries/DelfyLiquidityMathLibrary.sol';
+
+contract ExampleComputeLiquidityValue {
+    using SafeMath for uint256;
+
+    address public immutable factory;
+
+    constructor(address factory_) public {
+        factory = factory_;
+    }
+
+    // see DelfyLiquidityMathLibrary#getReservesAfterArbitrage
+    function getReservesAfterArbitrage(
+        address tokenA,
+        address tokenB,
+        uint256 truePriceTokenA,
+        uint256 truePriceTokenB
+    ) external view returns (uint256 reserveA, uint256 reserveB) {
+        return DelfyLiquidityMathLibrary.getReservesAfterArbitrage(
+            factory,
+            tokenA,
+            tokenB,
+            truePriceTokenA,
+            truePriceTokenB
+        );
+    }
+
+    // see DelfyLiquidityMathLibrary#getLiquidityValue
+    function getLiquidityValue(
+        address tokenA,
+        address tokenB,
+        uint256 liquidityAmount
+    ) external view returns (
+        uint256 tokenAAmount,
+        uint256 tokenBAmount
+    ) {
+        return DelfyLiquidityMathLibrary.getLiquidityValue(
+            factory,
+            tokenA,
+            tokenB,
+            liquidityAmount
+        );
+    }
+
+    // see DelfyLiquidityMathLibrary#getLiquidityValueAfterArbitrageToPrice
+    function getLiquidityValueAfterArbitrageToPrice(
+        address tokenA,
+        address tokenB,
+        uint256 truePriceTokenA,
+        uint256 truePriceTokenB,
+        uint256 liquidityAmount
+    ) external view returns (
+        uint256 tokenAAmount,
+        uint256 tokenBAmount
+    ) {
+        return DelfyLiquidityMathLibrary.getLiquidityValueAfterArbitrageToPrice(
+            factory,
+            tokenA,
+            tokenB,
+            truePriceTokenA,
+            truePriceTokenB,
+            liquidityAmount
+        );
+    }
+
+    // test function to measure the gas cost of the above function
+    function getGasCostOfGetLiquidityValueAfterArbitrageToPrice(
+        address tokenA,
+        address tokenB,
+        uint256 truePriceTokenA,
+        uint256 truePriceTokenB,
+        uint256 liquidityAmount
+    ) external view returns (
+        uint256
+    ) {
+        uint gasBefore = gasleft();
+        DelfyLiquidityMathLibrary.getLiquidityValueAfterArbitrageToPrice(
+            factory,
+            tokenA,
+            tokenB,
+            truePriceTokenA,
+            truePriceTokenB,
+            liquidityAmount
+        );
+        uint gasAfter = gasleft();
+        return gasBefore - gasAfter;
+    }
+}
